916eada7da5d2054c6493b8fea753fdaea316c12
Khellermann Machado <khellermann@gmail.com>
2019-08-31 14:57:20 -0300

    Chore: config model user and config database

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
+
+import User from "./app/models/User";
+
 const routes = new Router();
 
-routes.get("/home", (req, res) => {
-  return res.json({ message: "Código inicial" });
+routes.get("/user", async (req, res) => {
+  const user = await User.create({
+    name: "Khauê Machado",
+    email: "khaue@gmail.com.br",
+    password_hash: "123456"
+  });
+  return res.json(user);
 });
 
 export default routes;
